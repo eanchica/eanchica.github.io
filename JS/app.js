dcdbf6848b547df2ca845c194a0b4cfc44247a2f
eanchica <60475527+eanchica@users.noreply.github.com>
2020-02-03 14:13:30 -0500

    JS added

---
+++ JS/app.js
@@ -0,0 +1,22 @@
+
+
+// COMMENTS USED FOR REFERENCE
+
+
+
+
+const player = {
+    name: "USS Schwartz",
+    hull: 20,
+    firepower: 5,
+    accuracy: 0.7
+    attack:
+}
+
+
+class alienShip = {
+    name:"Alien Ship",
+    hull: 3-6,
+
+}
+ 
---
+++ README.md
@@ -0,0 +1,1 @@
+# eanchica.github.io
